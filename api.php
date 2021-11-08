babe58bdfb696efa84e980ac59b3069bd62831a9
Vit <415816@bk.ru>
2021-11-08 22:48:06 +0300

    update date of DB and site

--- api.php
+++ api.php
@@ -3,8 +3,8 @@
 	$FIO = $_POST['FIO'];
 	$score = $_POST['score'];
 	$ball = $_POST['ball'];
-	$dbh = new PDO('mysql:host=localhost;dbname=cb95298_db', 'cb95298_db', '5frWSN28y7we');
-	$stmt = $dbh->prepare("INSERT INTO `order`(`date`, FIO, score, ball)
+	$dbh = new PDO('mysql:host=localhost;dbname=cj33580_tbdp', 'cj33580TBDP', 'tz8OsLR3rWur');
+	$stmt = $dbh->prepare("INSERT INTO `order_tbdp`(`date`, FIO, score, ball)
 		VALUES (NOW(), :FIO, :score, :ball)");
 	$stmt->bindParam(':FIO', $FIO);
 	$stmt->bindParam(':score', $score);
